616637beacfeea47765fcef6ca0574a47b380a9c
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2022-07-13 19:58:23 -0500

    Modificacion

--- tickets-agente-backend/src/main/java/yavirac/tickets/feature/person/PersonService.java
+++ tickets-agente-backend/src/main/java/yavirac/tickets/feature/person/PersonService.java
@@ -1,4 +1,4 @@
-package yavirac.seguridad.feature.person;
+package yavirac.tickets.feature.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
