0264cceea41e031d7d355ed980d9cbe47bcc9d5a
unknown <akz.ibarra@yavirac.edu.ec>
2022-07-21 22:59:04 -0500

    Toolbar-list

--- tickets-agente-frontend/src/app/feature/feature.module.ts
+++ tickets-agente-frontend/src/app/feature/feature.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { FeatureRoutingModule } from './feature-routing.module';
 import { RolComponent } from './rol/rol.component';
-import { ModuloComponent } from './modulo/modulo.component';
 import { MotivoComponent } from './motivo/motivo.component';
 import { SugerenciaComponent } from './sugerencia/sugerencia.component';
 import { FormsModule } from '@angular/forms';
+import { ModuloFormComponent } from './modulo/form/modulo-form.component';
+import { ModuloListComponent } from './modulo/list/modulo-list.component';
+import { ModuloToolbarComponent } from './modulo/toolbar/modulo-toolbar.component';
 
 
 @NgModule({
   declarations: [
     RolComponent,
-    ModuloComponent,
+    ModuloListComponent,
+    ModuloFormComponent,
+    ModuloToolbarComponent,
     MotivoComponent,
     SugerenciaComponent
   ],
