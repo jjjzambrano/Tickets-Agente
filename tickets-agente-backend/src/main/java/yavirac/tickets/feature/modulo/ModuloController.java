02052954e39374748f2ed0c357bb375d3a767620
Joshtyn <jjj.zambrano@yavirac.edu.ec>
2022-07-31 13:51:27 -0500

    Modificacion

--- tickets-agente-backend/src/main/java/yavirac/tickets/feature/modulo/ModuloController.java
+++ tickets-agente-backend/src/main/java/yavirac/tickets/feature/modulo/ModuloController.java
@@ -3,9 +3,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import java.util.List;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
